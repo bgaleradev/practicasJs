7780e0e11c1c4e7263b7dde71aec78dfb91d4b89
bgaleradev <104566801+bgaleradev@users.noreply.github.com>
2022-04-29 11:58:08 +0200

    Add files via upload

---
+++ ejercicioTema2.js
@@ -0,0 +1,15 @@
+//Declaro e inicializo la primera variable
+let variable1 = "Soy la variable 1";
+
+//Declaro y creo la funcion para mostrar otra variable
+function mostrarVariable(){
+    let variable1 ="Ahora me llamo variable 2";
+    console.log(variable1);
+}
+
+//Se llama a la funcion anterior
+mostrarVariable();
+
+//Muestro por pantalla el contenido de la primera variable
+console.log(variable1);
+
