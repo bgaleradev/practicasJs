6a7dc4880ed104e8ae3c0040497111183ecb665c
bgaleradev <104566801+bgaleradev@users.noreply.github.com>
2022-04-29 18:37:00 +0200

    Add files via upload

---
+++ cadenasEjercicio1.js
@@ -0,0 +1,14 @@
+//Primer ejercicio de la seccion sobre cadenas de caracteres
+/*
+    Requisitos:
+    Crear una variable con una cadena.
+    Modificar una palabra de la cadena
+    Imprimir por consola la cadena modificada
+*/
+
+//Declaracion e inicializacion de la cadena
+let cadena1 = "El perro de mi vecina es muy molesto";
+//Modificacion de la cadena sobre la propia variable
+cadena1 = cadena1.replace("vecina", "vecino");
+//Mostrar la variable modificada por la terminal
+console.log(cadena1);
