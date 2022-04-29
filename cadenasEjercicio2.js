f76e43d5fb64d21f7e6ffad35afd756378c115e7
bgaleradev <104566801+bgaleradev@users.noreply.github.com>
2022-04-29 18:43:27 +0200

    Add files via upload

---
+++ cadenasEjercicio2.js
@@ -0,0 +1,13 @@
+//Segundo ejercicio de la seccion sobre cadenas de caracteres
+/*
+    Requisitos:
+        -Crear variable con una cadena de texto
+        -Convertir la cadena a mayusculas
+        -Mostrar por consola
+*/
+//Declaracion e inicializacion de la variable
+let cadena = "Esto es Una Cadena";
+//Convertir la cadena a mayusculas
+cadena = cadena.toUpperCase();
+//Mostrar por consola
+console.log(cadena);
