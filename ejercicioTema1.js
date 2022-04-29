396b771c9e3cf1d674b7f57ff4bc55224ad93ac5
bgaleradev <104566801+bgaleradev@users.noreply.github.com>
2022-04-29 10:29:10 +0200

    Ejercicio1

---
+++ ejercicioTema1.js
@@ -0,0 +1,7 @@
+//Primer ejercicio del curos de JavaScript en Open-bootcamp
+/*
+    Requisitos:
+    Mostrar por consola un mensaje.
+*/
+
+console.log("Estoy realizando el curso de JavaScript, espero aprender mucho");
